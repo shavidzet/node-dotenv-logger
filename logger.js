42cb1ed67cbc73080623397c9389a2b4faea36f4
Tornike <shavidzet@protonmail.com>
2019-07-20 06:17:22 +0400

    beautify and log output

--- logger.js
+++ logger.js
@@ -1,11 +1,19 @@
 ;(() => {
+  const beautify = require('json-beautify')
+
+  const logger = require('tracer').console()
+
   const env = require('dotenv').config({
     path: process.env.ENV_PATH || '.env'
   })
+
   /* eslint-disable no-console */
   const envs = {
     ...env,
     NODE_ENV: process.env.NODE_ENV
   }
-  console.log(envs)
+
+  const output = beautify(envs.parsed, null, 1, 30)
+
+  logger.log(output)
 })()
